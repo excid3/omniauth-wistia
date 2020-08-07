d466f8167bce3b8aec574039e8b52cef7121b761
Chris Oliver <excid3@gmail.com>
2020-08-07 13:11:27 -0500

    First commit

---
+++ lib/omniauth/wistia.rb
@@ -0,0 +1,8 @@
+require "omniauth/wistia/version"
+
+module Omniauth
+  module Wistia
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/wistia/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Wistia
+    VERSION = "0.1.0"
+  end
+end
