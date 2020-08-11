5c3494cf92526260e85df303eef8a226a20dc563
Chris Oliver <excid3@gmail.com>
2020-08-11 14:22:33 -0500

    Fix uid and info and standardize

--- lib/omniauth/strategies/wistia.rb
+++ lib/omniauth/strategies/wistia.rb
@@ -1,27 +1,37 @@
-require 'omniauth-oauth2'
-require 'multi_json'
+require "omniauth-oauth2"
+require "multi_json"
 
 module OmniAuth
   module Strategies
     class Wistia < OmniAuth::Strategies::OAuth2
-      DEFAULT_SCOPE = 'all:all'
+      DEFAULT_SCOPE = "all:all"
 
-      option :name, 'wistia'
+      option :name, "wistia"
 
       option :client_options, {
-        :site => 'https://api.wistia.com',
-        :authorize_path => '/oauth/authorize',
-        :token_url => '/oauth/token',
+        site: "https://api.wistia.com",
+        authorize_path: "/oauth/authorize",
+        token_url: "/oauth/token"
       }
+
+      uid do
+        raw_info["id"]
+      end
+
+      info do
+        {
+          name: raw_info["name"]
+        }
+      end
 
       extra do
         {
-          'raw_info' => raw_info
+          "raw_info" => raw_info
         }
       end
 
       def raw_info
-        @raw_info ||= access_token.get('https://api.wistia.com/v1/account.json').parsed
+        @raw_info ||= access_token.get("https://api.wistia.com/v1/account.json").parsed
       end
 
       protected
@@ -30,7 +40,6 @@ module OmniAuth
       def callback_url
         full_host + script_name + callback_path
       end
-
     end
   end
 end
